c85ca75f8192dde0bbc9b4aced06ae2818091250
Fábio Notaro <fabionotarofilho@gmail.com>
2024-08-27 09:26:43 -0300

    removendo coisas desnecessarias

--- Admin/routes/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RoutingController;
-
-Route::match(['GET', 'POST'], '{pagina?}/{id?}/{extra?}/{extra2?}/{extra3?}/{extra4?}', [RoutingController::class, 'index']);
