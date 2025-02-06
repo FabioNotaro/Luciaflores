b0a25f05ca954a2dc7cfbd5ca5349ca5fe6155fc
Fabio Notaro <fabio@letsevolve.com.br>
2025-02-06 10:00:51 -0300

    ajustes

--- project/database/migrations/2014_10_12_070000_create_users_table.php
+++ project/database/migrations/2014_10_12_070000_create_users_table.php
@@ -10,33 +10,33 @@ return new class extends Migration
      * Run the migrations.
      */
     
-    public function up(){
-        // Primeiro, crie a tabela 'users'
-        if (!Schema::hasTable('users')) {
-            Schema::create('users', function (Blueprint $table) {
-                $table->id();  // Cria a coluna 'id' como unsignedBigInteger (auto-increment)
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->timestamp('email_verified_at')->nullable();
-                $table->string('password');
-                $table->rememberToken();
-                $table->timestamps();
-            });
-        }
-    
-        // Depois, crie a tabela 'sessions'
-        Schema::dropIfExists('sessions');
-        if (!Schema::hasTable('sessions')) {
-            Schema::create('sessions', function (Blueprint $table) {
-                $table->string('id', 191)->primary();
-                $table->foreignId('user_id')->constrained()->onDelete('cascade'); // A chave estrangeira para a tabela 'users'
-                $table->string('ip_address', 45)->nullable();
-                $table->text('user_agent')->nullable();
-                $table->longText('payload');
-                $table->unsignedBigInteger('last_activity')->index();
-            });
-        }
-    }
+     public function up(){
+         // Criação da tabela 'users' (se não existir)
+         if (!Schema::hasTable('users')) {
+             Schema::create('users', function (Blueprint $table) {
+                 $table->id();  // 'id' como unsignedBigInteger (auto-increment)
+                 $table->string('name');
+                 $table->string('email')->unique();
+                 $table->timestamp('email_verified_at')->nullable();
+                 $table->string('password');
+                 $table->rememberToken();
+                 $table->timestamps();
+             });
+         }
+     
+         // Criação da tabela 'sessions' (se não existir)
+         Schema::dropIfExists('sessions');
+         if (!Schema::hasTable('sessions')) {
+             Schema::create('sessions', function (Blueprint $table) {
+                 $table->string('id', 191)->primary(); // A chave primária
+                 $table->foreignId('user_id')->constrained('users', 'id')->onDelete('cascade'); // Definindo a chave estrangeira corretamente
+                 $table->string('ip_address', 45)->nullable();
+                 $table->text('user_agent')->nullable();
+                 $table->longText('payload');
+                 $table->unsignedBigInteger('last_activity')->index();
+             });
+         }
+     }
 
     /**
      * Reverse the migrations.
